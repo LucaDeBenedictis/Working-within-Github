4cb6d2b897ac00f57a9e72da257dea68d1ca9137
Luca De Benedictis <luca.debenedictis@unimc.it>
2022-11-21 18:14:09 +0100

    Configure Git

---
+++ Working within Github.R
@@ -0,0 +1,29 @@
+# How to Use Git/GitHub with R
+
+# from https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
+
+# Reference: https://happygitwithr.com/index.html
+
+# (1) Insall Github
+
+# (2) Check that Github has been propperly installed
+
+# Go to terminal
+# MacBookedictis2:Working-within-Github debene2$ 
+# type: which git 
+# /usr/local/bin/git
+# type: git --version
+# git version 2.35.1
+
+# YES! Git installation is verified
+
+
+
+# Configure Git (Chapter 7 of Happy Git with R)
+
+library(usethis)
+edit_git_config()
+
+# [user]
+# name = Luca De Benedictis
+# email = luca.debenedictis@unimc.it
